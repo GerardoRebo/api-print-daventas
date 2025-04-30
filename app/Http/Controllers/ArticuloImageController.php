d317d7869eeca0ff3909486288880d3d3eb2ca39
GerardoRebo <81606616+GerardoRebo@users.noreply.github.com>
2025-04-30 00:01:37 -0600

    add images to articulos

--- app/Http/Controllers/ArticuloImageController.php
+++ app/Http/Controllers/ArticuloImageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ArticuloFile;
 use App\Models\VentaticketArticulo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Storage;
 
 class ArticuloImageController extends Controller
@@ -20,10 +21,10 @@ class ArticuloImageController extends Controller
             'files' => 'required|array',
             'files.*' => 'file|mimes:jpg,jpeg,png,pdf,psd|max:10240', // max 10MB
         ]);
+        $disk = App::environment('production') ? 's3' : 'public';
 
         foreach ($request->file('files') as $uploadedFile) {
-            $path = $uploadedFile->store('public/articulos', 'public');
-
+            $path = $uploadedFile->store('articulos', $disk);
             $articulo->files()->create([
                 'filename' => $uploadedFile->getClientOriginalName(),
                 'path' => $path,
