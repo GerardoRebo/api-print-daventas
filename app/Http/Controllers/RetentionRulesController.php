2943aff0b6a609e798b3503d5f39f8fd7a57a386
GerardoRebo <81606616+GerardoRebo@users.noreply.github.com>
2025-08-31 11:44:17 -0600

    retention

--- app/Http/Controllers/RetentionRulesController.php
+++ app/Http/Controllers/RetentionRulesController.php
@@ -5,34 +5,47 @@ namespace App\Http\Controllers;
 use App\Models\RetentionRule;
 use App\Models\Tax;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RetentionRulesController extends Controller
 {
     function index()
     {
+        if (request()->query('type') == 'common') {
+            return config('retention_rules');
+        }
         $user = request()->user();
-        return RetentionRule::with('tax')->where('organization_id', $user->organization_id)->get();
+        return RetentionRule::with('tax')
+            ->where('organization_id', $user->organization_id)
+            ->get();
     }
     function store()
     {
         $user = request()->user();
 
         $validated = request()->validate([
-            'tax_id' => "required",
-            'regimen_fiscal' => "numeric",
+            'name' => "required",
+            'regimen_fiscal' => "nullable",
+            'isr_percentage' => "nullable",
+            'iva_percentage' => "nullable|numeric",
         ]);
+
         $validated['organization_id'] = $user->organization_id;
         RetentionRule::create($validated);
     }
     function update($id)
     {
         $validated = request()->validate([
-            'tax_id' => "required",
-            'regimen_fiscal' => "numeric",
+            'name' => "required",
+            'regimen_fiscal' => "nullable",
+            'isr_percentage' => "nullable",
+            'iva_percentage' => "nullable|numeric",
         ]);
         $retentionRule = RetentionRule::findOrFail($id);
-        $retentionRule->tax_id = $validated['tax_id'];
+        $retentionRule->name = $validated['name'];
         $retentionRule->regimen_fiscal = $validated['regimen_fiscal'];
+        $retentionRule->isr_percentage = $validated['isr_percentage'];
+        $retentionRule->iva_percentage = $validated['iva_percentage'];
         $retentionRule->save();
     }
     function destroy($id)
---
+++ config/retention_rules.php
@@ -0,0 +1,45 @@
+<?php
+return [
+    1 => [
+        "name" => 'RESICO con Actividad Empresarial',
+        'aplies_isr' => true,
+        'isr_percentage' => '1.25',
+        'aplies_iva' => false,
+        'iva_percentage' => null,
+    ],
+    2 => [
+        "name" => 'RESICO con actividad Profesional (Honorarios)',
+        'aplies_isr' => true,
+        'isr_percentage' => '1.25',
+        'aplies_iva' => true,
+        'iva_percentage' => '10.666666',
+    ],
+    3 => [
+        "name" => 'RESICO en arrendamiento casa habitación',
+        'aplies_isr' => true,
+        'isr_percentage' => '1.25',
+        'aplies_iva' => false,
+        'iva_percentage' => null,
+    ],
+    4 => [
+        "name" => 'RESICO en arrendamiento otros inmuebles',
+        'aplies_isr' => true,
+        'isr_percentage' => '1.25',
+        'aplies_iva' => true,
+        'iva_percentage' => '10.666666',
+    ],
+    5 => [
+        "name" => 'RESICO como agapes',
+        'aplies_isr' => true,
+        'isr_percentage' => '1.25',
+        'aplies_iva' => false,
+        'iva_percentage' => null,
+    ],
+    6 => [
+        "name" => 'RESICO en autotransporte de bienes',
+        'aplies_isr' => true,
+        'isr_percentage' => '1.25',
+        'aplies_iva' => true,
+        'iva_percentage' => '4',
+    ],
+];
